74778ab6af7e1ad89bc7fc701afcfcfe06527470
Giorgi Kalandadze <giorgikalandadze24@gmail.com>
2024-01-27 17:58:35 +0400

    add result codes as constant

---
+++ server/src/contsants.js
@@ -0,0 +1,13 @@
+const RESULT_CODES = {
+    SUCCESS: 0,
+    ERROR: -1,
+    WARNING: 1,
+};
+
+const RESULT_STATUSES = {
+    SUCCESS: 'SUCCESS',
+    ERROR: 'ERROR',
+    WARNING: 'WARNING',
+}
+
+module.exports = {RESULT_CODES, RESULT_STATUSES};
